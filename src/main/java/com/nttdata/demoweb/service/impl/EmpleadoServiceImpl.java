c5f7664b5486c2667c9b00855aaec0af30005c2a
thelich <javilpx2.jl@gmail.com>
2022-03-10 11:12:36 +0100

    Inyección de dependencias

---
+++ src/main/java/com/nttdata/demoweb/service/impl/EmpleadoServiceImpl.java
@@ -0,0 +1,19 @@
+package com.nttdata.demoweb.service.impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.nttdata.demoweb.repository.EmpleadoRepo;
+import com.nttdata.demoweb.service.EmpleadoService;
+
+
+@Service
+public class EmpleadoServiceImpl implements EmpleadoService {
+	
+	@Autowired
+	EmpleadoRepo empleadoRepo;
+	
+	
+	@Override
+	public void registrar(String name) {
+		empleadoRepo.registrar(name);
+	}
+}
